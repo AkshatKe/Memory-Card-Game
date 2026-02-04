6a43ca6c99da19e9fe8db8b1e45cf33de2e9699f
AkshatKe <lkus74424@gmail.com>
2026-02-04 22:57:42 +0530

    Change Style

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
             for (var i = 0; i < cardImages.length; i++) {
                 var card = document.createElement('div');
                 card.className = 'card';
-                card.innerHTML = '<div class="card-front"><i class="fas fa-heart"></i></div>' +
+                card.innerHTML = '<div class="card-front"><i class="fas fa-diamond"></i></div>' +
                                 '<div class="card-back"><img src="' + cardImages[i] + '"></div>';
                 card.onclick = flipCard;
                 card.dataset.image = cardImages[i];
